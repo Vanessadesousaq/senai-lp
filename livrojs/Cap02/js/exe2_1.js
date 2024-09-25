f6dbf1fa209e77b80d6517c9764b561fdd07a21d
Vanessa <vanessadesousaq@gmail.com>
2024-09-24 21:50:56 -0300

    exemplo de exercicio cap 02

---
+++ livrojs/Cap02/js/exe2_1.js
@@ -0,0 +1,10 @@
+//criar uma referencia ao form e ao h3 (onde sera exibida a resposta )
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+//cria um 'ouvinte' de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit",(e)=>{
+    const nome = frm.inNome.value; //obtem o nome digitado no form
+    resp.innerText = `Olá ${nome}`; //exibe a resposta no programa 
+    e.preventDefault();//vai evitar que o form envie od dados pro destino.php
+})
